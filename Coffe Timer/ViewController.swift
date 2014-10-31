27b1727bd79d2df3abc258fb817302a1bef819ff
João Antonio Ferreira <joao.parana@gmail.com>
2014-10-31 16:06:09 -0200

    Versão Inicial

---
+++ Coffe Timer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Coffe Timer
+//
+//  Created by Joao Ferreira on 10/30/14.
+//  Copyright (c) 2014 Joao Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        println("View is loaded.")
+        view.backgroundColor = UIColor.orangeColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
