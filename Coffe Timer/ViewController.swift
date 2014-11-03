711cfb0ff8b4ade9e5a07a13c558e49c630f0ae0
João Antonio Ferreira <joao.parana@gmal.com>
2014-11-03 20:15:30 -0200

    botão e label adicionandos

--- Coffe Timer/ViewController.swift
+++ Coffe Timer/ViewController.swift
@@ -9,6 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+   
+    @IBOutlet weak var label: UILabel!
+    @IBAction func buttonWasPressed(sender: UIButton) {
+        println("Button was pressed.")
+        // Get the current date and time
+        let formatter  = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        let todayDate = formatter.dateFromString("2014-10-30")!
+        let date = todayDate
+        
+        // Update the label
+        label.text = "date : \(date)"
+
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +36,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func getDayOfWeek(today:String)->Int {
+        
+        let formatter  = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        let todayDate = formatter.dateFromString(today)!
+        let myCalendar = NSCalendar(calendarIdentifier: NSGregorianCalendar)
+        let myComponents = myCalendar?.components(.WeekdayCalendarUnit, fromDate: todayDate)
+        let weekDay = myComponents?.weekday
+        return weekDay!
+    }
 
 }
 
